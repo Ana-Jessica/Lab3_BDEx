2647c9884f64e6e137a5306db45862c2c323121f
Roberto Eduardo <robertoemd@gmail.com>
2025-05-21 10:24:30 -0300

    :hammer: mudanças no layout do index.html

---
+++ static/scripts/preloader.js
@@ -0,0 +1,8 @@
+
+    window.addEventListener("load", function () {
+        setTimeout(function () {
+            document.getElementById("preloader").style.display = "none";
+            document.querySelector(".container").style.display = "block";
+        }, 3000); // 3000 milissegundos = 3 segundos
+    });
+
