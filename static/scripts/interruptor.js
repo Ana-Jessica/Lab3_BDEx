7fb49255e6c71a9519c99dcf33bb6e301f8ffb0b
giulialorrany <giulialourrany@gmail.com>
2025-06-02 21:34:45 -0300

    projeto com symfony e alterações desativar e newSenhaEmail

--- static/scripts/interruptor.js
+++
@@ -1,4 +0,0 @@
-    const toggle = document.getElementById('themeToggle');
-    toggle.addEventListener('change', function() {
-      document.body.classList.toggle('dark-mode');
-    });
--- static/scripts/preloader.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-  const preloader = document.querySelector(".conteiner_preloader");
-  if (preloader) {
-    setTimeout(() => {
-      preloader.style.display = "none";
-    }, 2000); 
-  }
-});
